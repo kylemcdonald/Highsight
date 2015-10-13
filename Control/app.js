9a66ec57b5eb0e58ab7e860fbb8a16daa9c98635
Ranjit Bhatnagar <ranjit@moonmilk.com>
2015-10-13 13:03:00 +0200

    updates from pioneer works

--- Control/app.js
+++ Control/app.js
@@ -5,9 +5,9 @@ var app = express();
 var revolutionsPerMeter = 16.818;
 var encoderResolution = 256;
 var countsPerMeter = revolutionsPerMeter * encoderResolution;
-var top = Math.round(countsPerMeter * 9.25); // 14.16meters from floor at 120,000 = ~8611 counts per meter
+var top = Math.round(countsPerMeter * 4.6);// 9.25 // 14.16meters from floor at 120,000 = ~8611 counts per meter
 var bottom = Math.round(countsPerMeter * 0.10);
-var slows = 800;
+var slows = 200;
 var slowac = 2000;
 var fasts = 1300; // 1300 is quite reliable, 1400 and 1350 will crash if loop error is set to 500ms
 var fastac = 15000; // 15000 
@@ -22,7 +22,7 @@ var server = app.listen(process.env.PORT || 3000, function () {
 	console.log('Listening at http://%s:%s', host, port);
 });
 
-roboteq.connect();
+roboteq.connect(); // should pass encoder resolution here
 
 app.get('/roboteq/open', function(req, res) {
 	res.json({
@@ -49,9 +49,44 @@ app.get('/roboteq/go/up', function(req, res) {
 	res.sendStatus(200);
 })
 
+app.get('/roboteq/get/speed', function(req, res) {
+  roboteq.getSpeed(function(result) {
+    res.json({'speed': result});
+  })
+})
+
 app.get('/roboteq/get/position', function(req, res) {
-	roboteq.getPosition(function(result) {
-		console.log('got robo result: ' + result);
-		res.json({'encoderCounterAbsolute': result});
-	})
+  roboteq.getPosition(function(result) {
+    res.json({'position': result});
+  })
+})
+
+app.get('/roboteq/get/volts', function(req, res) {
+  roboteq.getVolts(function(result) {
+    res.json({'volts': result});
+  })
+})
+
+app.get('/roboteq/get/motor/amps', function(req, res) {
+  roboteq.getMotorAmps(function(result) {
+    res.json({'amps': result});
+  })
+})
+
+app.get('/roboteq/get/battery/amps', function(req, res) {
+  roboteq.getBatteryAmps(function(result) {
+    res.json({'amps': result});
+  })
+})
+
+app.get('/roboteq/get/destinationReached', function(req, res) {
+  roboteq.getDestinationReached(function(result) {
+    res.json({'destinationReached': result});
+  })
+})
+
+app.get('/roboteq/set/echo', function(req, res) {
+  var enable = (req.query.echo == 'true');
+  roboteq.setEcho(enable);
+  res.sendStatus(200);
 })
--- Control/roboteq.js
+++ Control/roboteq.js
@@ -5,13 +5,17 @@ var serialport = require('serialport');
 
 function getComName(cb) {
 	serialport.list(function(err, ports) {
-		ports.forEach(function(port) {
+		var found = ports.some(function(port) {
 			if(port.manufacturer == 'Roboteq') {
 				cb(null, port.comName);
-				return;
+				return true;				
+			} else {
+  			return false;
 			}
 		})
-		cb('Cannot find matching port.');
+		if(!found) {
+  		cb('Cannot find matching port.');
+		}
 	})
 }
 
@@ -24,6 +28,18 @@ function updateSerialStatus(status) {
 	serialStatus = status;
 }
 
+var dataCallbackHook;
+function dataCallback(data) {
+	console.log('data: ' + data);
+	if(data == '+' || data == '-') {
+  	console.log('ignoring callback');
+  	return;
+	}
+  if(dataCallbackHook) {
+    dataCallbackHook(data);
+    delete dataCallbackHook;
+  }
+}
 exports.connect = function() {
 	if(serial && serial.isOpen()) return;
 	getComName(function(err, comName) {
@@ -40,6 +56,7 @@ exports.connect = function() {
 				reconnect();
 			}
 		}, false);
+		serial.on('data', dataCallback);
 		serial.open(function(err) {
 			if(err) {
 				updateSerialStatus('error connecting, reconnecting');
@@ -67,10 +84,16 @@ exports.command = function(command) {
 		console.log('Ignored command: ' + command);
 		return;
 	}
-	serial.write(command + '\r', function(err, results) {
-		if(err) console.log('err ' + err);
-		if(results) console.log('results ' + results);
-	});
+	write(command + '\r');
+}
+
+function write(command, cb) {
+  if(cb) {
+    dataCallbackHook = cb;
+  }
+  serial.write(command, function(err) {
+		if(err) console.log('err ' + err);    
+  })
 }
 
 exports.query = function(query, cb) {
@@ -79,19 +102,9 @@ exports.query = function(query, cb) {
 		cb();
 		return;
 	}
-	serial.on('data', function(data) {
-		if(!data) {
-			console.log('data event with no data');
-			return;
-		}
-		console.log('got data: ' + data);
-		var parts = data.split('=')[0];
-		var type = parts[0].toLowerCase();
-		cb(type, parts[1]);
-	});
-	serial.write(query + '\r', function(err, results) {
-		if(err) console.log('err ' + err);
-		if(results) console.log('results ' + results);
+	write(query + '\r', function(data) {
+		var parts = data.split('=');
+		cb(parts[1]);
 	});
 }
 
@@ -100,9 +113,16 @@ exports.query = function(query, cb) {
 // general notes (p 115):
 // - commands are not case sensitive
 // - commands are terminated by carriage return (hex 0x0d, '\r')
-// - controlloer will echo every command it receives
+// - controller will echo every command it receives
 // - for commands where no reply is expected, it will return a '+' character
 // - for bad commands, it will return a '-' character
+exports.setEcho = function(enable) {
+  if(enable) {
+    exports.command('^echof 0');
+  } else {
+    exports.command('^echof 1');
+  }
+}
 exports.setAcceleration = function(acceleration) {
 	exports.command('!ac 1 ' + acceleration);
 }
@@ -118,21 +138,30 @@ exports.setPosition = function(position) {
 exports.getPosition = function(cb) {
 	exports.query('?c 1', cb); // also called "encoder counter absolute"
 }
-exports.getSpeed = function(cb) {
+exports.getSpeed = function(cb) { // units are .1 * RPM / s, called "set velocity" in manual
 	exports.query('?s 1', cb);
 }
+// returns internal voltage * 10 : main battery voltage * 10 : v5out on dsub in millivolts, see p 186
 exports.getVolts = function(cb) {
-	exports.query('?v 1', cb); // returns internal voltage * 10 : main battery voltage * 10 : v5out on dsub in millivolts, see p 186
+	exports.query('?v 1', function(volts) {
+  	cb(volts / 10);  	
+	});
 }
 exports.getMotorAmps = function(cb) { // returns units of amps * 10, p 173
-	exports.query('?A 1', cb);
+	exports.query('?A 1', function(amps) {
+  	cb(amps / 10);  	
+	});
 }
 exports.getBatteryAmps = function(cb) { // returns units of amps * 10, p 175
-	exports.query('?BA 1', cb);
+	exports.query('?BA 1', function(amps) {
+  	cb(amps / 10);	
+	})
 }
 exports.getDestinationReached = function(cb) { // p 179, p 104
 	exports.query('?DR 1', cb);
 }
+
+// follow commands are untested
 exports.getFaults = function(cb) {
 	exports.query('?FF 1', cb); // see p 180 for the meaning of each bit
 }
