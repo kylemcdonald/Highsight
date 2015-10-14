50a758529da2e4f6445481336e1cbf36f232a6e9
Kyle McDonald <kyle@kylemcdonald.net>
2015-10-14 19:35:29 +0200

    moved to winston for logging

--- Control/app.js
+++ Control/app.js
@@ -1,9 +1,18 @@
 // guideline: pressing any button should always be safe
 
 // if there is ever no current we need to stop.. need to check multiple times though
-// add more logs for state
 // rewriting serial to handle out of order messages (and with echo enabled), chart them in realtime
 // realtime preview from OF to iPad
+// make nodemon start without password?
+// return to bottom after enough time has passed
+
+var winston = require('winston');
+var logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({ level: 'info' }),
+    new (winston.transports.File)({ filename: 'logfile.log', level: 'debug' })
+  ]
+});
 
 var roboteq = require('./roboteq.js');
 var osc = require('osc');
@@ -16,6 +25,7 @@ var minimumVoltage = 23;
 var countsPerMeter = revolutionsPerMeter * encoderResolution
 var nudgeAmount = 0.10;
 var safeDistance = 0.05;
+var returnToBottomDuration = 1000 * 60 * 5; // time after last transition to return to bottom
 var boxs = 10;
 var boxac = 100;
 var boxdc = 100;
@@ -27,39 +37,39 @@ var fastac = 15000;
 var fastdc = 12000;
 
 function metersToEncoderUnits(meters) {
-	return Math.round(countsPerMeter * meters)
+  return Math.round(countsPerMeter * meters)
 }
 
 function encoderUnitsToMeters(encoderUnits) {
-	return encoderUnits / countsPerMeter;
+  return encoderUnits / countsPerMeter;
 }
 
 var positions = {
-	'top': 11.5,
-	'boxtop': 10.05,
-	'boxview': 9.5,
-	'boxbottom': 8.9,
-	'openair': 8.5,
-	'myself': 2.0,
-	'bottom': 0.1
+  'top': 11.5,
+  'boxtop': 10.05,
+  'boxview': 9.5,
+  'boxbottom': 8.9,
+  'openair': 8.5,
+  'myself': 2.0,
+  'bottom': 0.1
 };
 
 var transitionDefault = {
-	s: slows,
-	ac: slowac,
-	dc: slowdc
+  s: slows,
+  ac: slowac,
+  dc: slowdc
 }
 
 var transitions = {
-	'shutdown': {end: 'bottom'},
-	'top': {end: 'top'},
-	'bottom': {end: 'bottom'},
-	'scene1': {start: 'top', end: 'boxtop'},
-	'scene2': {start: 'boxtop', end: 'boxview', s: boxs, ac: boxac, dc: boxdc},
-	'scene3': {start: 'boxview', end: 'boxbottom', s: boxs, ac: boxac, dc: boxdc},
-	'scene4': {start: 'boxbottom', end: 'openair'},
-	'scene5': {start: 'openair', end: 'myself', s: fasts, ac: fastac, dc: fastdc}, // should be fast
-	'scene6': {start: 'myself', end: 'openair'}, // should be slow
+  'shutdown': {end: 'bottom'},
+  'top': {end: 'top'},
+  'bottom': {end: 'bottom'},
+  'scene1': {start: 'top', end: 'boxtop'},
+  'scene2': {start: 'boxtop', end: 'boxview', s: boxs, ac: boxac, dc: boxdc},
+  'scene3': {start: 'boxview', end: 'boxbottom', s: boxs, ac: boxac, dc: boxdc},
+  'scene4': {start: 'boxbottom', end: 'openair'},
+  'scene5': {start: 'openair', end: 'myself', s: fasts, ac: fastac, dc: fastdc}, // should be fast
+  'scene6': {start: 'myself', end: 'openair'}, // should be slow
 };
 
 app.use(express.static(__dirname + '/public'));
@@ -71,111 +81,125 @@ var udpPort = new osc.UDPPort({
 udpPort.open();
 
 var server = app.listen(process.env.PORT || 3000, function () {
-	var host = server.address().address;
-	var port = server.address().port;
-	console.log('Listening at http://%s:%s', host, port);
+  var host = server.address().address;
+  var port = server.address().port;
+  logger.info('Listening at http://%s:%s', host, port);
 });
 
 // could pass encoder resolution here?
 roboteq.connect({
-	// these are limited internally as an error-check
-	top: metersToEncoderUnits(11.5),
-	bottom: metersToEncoderUnits(0.1),
-	speedLimit: 1400,
-	accelerationLimit: 15000,
-	decelerationLimit: 12000
+  // these are limited internally as an error-check
+  top: metersToEncoderUnits(11.5),
+  bottom: metersToEncoderUnits(0.1),
+  speedLimit: 1400,
+  accelerationLimit: 15000,
+  decelerationLimit: 12000
 });
 
 app.get('/roboteq/open', function(req, res) {
-	res.json({
-		'status': active ? roboteq.serialStatus() : 'Low Voltage',
-		'open': roboteq.isOpen()
-	})
+  res.json({
+    'status': active ? roboteq.serialStatus() : 'Low Voltage',
+    'open': roboteq.isOpen()
+  })
 })
 
 setInterval(function() {
-	if(active) {
-		roboteq.getVolts(function(volts) {
-			if(volts < minimumVoltage) {
-				console.log('Past minimum voltage, shutting down.');
-				safeApplyTransition('shutdown');
-			}
-		})
-	}
+  if(active) {
+    roboteq.getVolts(function(volts) {
+      if(volts) {
+        logger.debug('getVolts', {volts: volts});
+        if(volts < minimumVoltage) {
+          logger.warn('Past minimum voltage, shutting down.');
+          safeApplyTransition('shutdown');
+        }
+      }
+    })
+  }
 }, 1000);
 
+var returnToBottomTimeout;
 function applyTransition(transitionName) {
-	var transition = transitions[transitionName];
-	roboteq.setSpeed(transition.s || transitionDefault.s);
-	roboteq.setAcceleration(transition.ac || transitionDefault.ac);
-	roboteq.setDeceleration(transition.dc || transitionDefault.dc);
-	var endPosition = positions[transition.end];
-	roboteq.setPosition(metersToEncoderUnits(endPosition));
+  if(returnToBottomTimeout) {
+    clearTimeout(returnToBottomTimeout);
+  }
+  var transition = transitions[transitionName];
+  roboteq.setSpeed(transition.s || transitionDefault.s);
+  roboteq.setAcceleration(transition.ac || transitionDefault.ac);
+  roboteq.setDeceleration(transition.dc || transitionDefault.dc);
+  var endPosition = positions[transition.end];
+  roboteq.setPosition(metersToEncoderUnits(endPosition));
+  returnToBottomTimeout = setTimeout(function() {
+    safeApplyTransition('bottom');
+    winston.log('Returning to bottom');
+  }, returnToBottomDuration);
 }
 
 function getSafeTransitions(cb) {
-	if(!active) {
-		return [];
-	}
-	roboteq.getPosition(function(positionEncoder) {
-		var positionMeters = encoderUnitsToMeters(positionEncoder);
-		var safeTransitions = [];
-		for(transitionName in transitions) {
-			var transition = transitions[transitionName];
-			var startPositionName = transition.start;
-			if(startPositionName) {
-				var startPositionMeters = positions[startPositionName];
-				if(Math.abs(positionMeters - startPositionMeters) < safeDistance) {
-					safeTransitions.push(transitionName);
-				}
-			} else {
-				safeTransitions.push(transitionName);
-			}
-		}
-		cb(safeTransitions);
-	})
+  if(!active) {
+    return [];
+  }
+  roboteq.getPosition(function(positionEncoder) {
+    if(positionEncoder) {
+      var positionMeters = encoderUnitsToMeters(positionEncoder);
+      logger.debug('getPosition', {positionMeters: positionMeters});
+      var safeTransitions = [];
+      for(transitionName in transitions) {
+        var transition = transitions[transitionName];
+        var startPositionName = transition.start;
+        if(startPositionName) {
+          var startPositionMeters = positions[startPositionName];
+          if(Math.abs(positionMeters - startPositionMeters) < safeDistance) {
+            safeTransitions.push(transitionName);
+          }
+        } else {
+          safeTransitions.push(transitionName);
+        }
+      }
+      cb(safeTransitions);
+    }
+  })
 }
 
 var active = true;
 function safeApplyTransition(transitionName) {
-	if(active) {
-		if(transitionName == 'shutdown') {
-			active = false;
-		}
-		getSafeTransitions(function(safe) {
-			console.log('checking for ' + transitionName + ' in ' + safe);
-			if(safe.indexOf(transitionName) > -1) {
-				applyTransition(transitionName);
-			} else {
-				console.log('ignoring unsafe call to safeApplyTransition(' + transitionName + ')');			
-			}
-		})
-	}
+  if(active) {
+    if(transitionName == 'shutdown') {
+      active = false;
+    }
+    getSafeTransitions(function(safe) {
+      if(safe.indexOf(transitionName) > -1) {
+        logger.verbose('Applying safe transition: ' + transitionName);
+        applyTransition(transitionName);
+      } else {
+        logger.warn('Ignoring unsafe transition: ' + transitionName);      
+      }
+    })
+  }
 }
 
 app.get('/roboteq/transition', function(req, res) {
-	var name = req.query.name;
-	console.log('/roboteq/transition to ' + name);
-	safeApplyTransition(name);
-	res.sendStatus(200);
+  var name = req.query.name;
+  logger.verbose('/roboteq/transition to ' + name);
+  safeApplyTransition(name);
+  res.sendStatus(200);
 })
 
 app.get('/roboteq/nudge/up', function(req, res) {
-	console.log('/roboteq/nudge/up');
-	roboteq.setSpeed(slows);
-	roboteq.setAcceleration(slowac);
-	roboteq.setDeceleration(slowdc);
-	roboteq.setPositionRelative(+metersToEncoderUnits(nudgeAmount));
-	res.sendStatus(200);
+  logger.verbose('/roboteq/nudge/up');
+  roboteq.setSpeed(slows);
+  roboteq.setAcceleration(slowac);
+  roboteq.setDeceleration(slowdc);
+  roboteq.setPositionRelative(+metersToEncoderUnits(nudgeAmount));
+  res.sendStatus(200);
 })
 
 app.get('/roboteq/nudge/down', function(req, res) {
-	console.log('/roboteq/nudge/down');	
-	roboteq.setSpeed(slows);
-	roboteq.setAcceleration(slowac);
-	roboteq.setDeceleration(slowdc);
-	roboteq.setPositionRelative(-metersToEncoderUnits(nudgeAmount));
-	res.sendStatus(200);
+  logger.verbose('/roboteq/nudge/down'); 
+  roboteq.setSpeed(slows);
+  roboteq.setAcceleration(slowac);
+  roboteq.setDeceleration(slowdc);
+  roboteq.setPositionRelative(-metersToEncoderUnits(nudgeAmount));
+  res.sendStatus(200);
 })
 
 app.get('/roboteq/get/speed', function(req, res) {
@@ -221,28 +245,31 @@ app.get('/roboteq/set/echo', function(req, res) {
 })
 
 app.get('/transitions/safe', function(req, res) {
-	getSafeTransitions(function(safeTransitions) {
-		res.json(safeTransitions);
-	})
+  getSafeTransitions(function(safeTransitions) {
+    res.json(safeTransitions);
+  })
 })
 
 app.get('/transitions/all', function(req, res) {
-	res.json(Object.keys(transitions));
+  res.json(Object.keys(transitions));
 })
 
 function sendOsc(address, args) {
-	udpPort.send({
-	    address: address,
-	    args: args
-	}, 'localhost', 9000);
+  udpPort.send({
+    address: address,
+    args: args
+  }, 'localhost', 9000);
 }
 
 app.get('/oculus/lookAngle/add', function (req, res) {
-	sendOsc('/lookAngle/add', [Number(req.query.value)]);
-	res.sendStatus(200);
+  var angle = Number(req.query.value);
+  logger.verbose('/oculus/lookAngle/add', {angle: angle})
+  sendOsc('/lookAngle/add', [angle]);
+  res.sendStatus(200);
 })
 
 app.get('/oculus/screenshot', function (req, res) {
-	sendOsc('/screenshot');
-	res.sendStatus(200);
+  logger.verbose('/screenshot');
+  sendOsc('/screenshot');
+  res.sendStatus(200);
 })
--- Control/roboteq.js
+++ Control/roboteq.js
@@ -4,138 +4,138 @@
 var serialport = require('serialport');
 
 function clamp(x, low, high) {
-	if(x < low) return low;
-	if(x > high) return high;
-	return x;
+  if(x < low) return low;
+  if(x > high) return high;
+  return x;
 }
 
 function getComName(cb) {
-	serialport.list(function(err, ports) {
-		var found = ports.some(function(port) {
-			if(port.manufacturer == 'Roboteq') {
-				cb(null, port.comName);
-				return true;				
-			} else {
-  			return false;
-			}
-		})
-		if(!found) {
-  		cb('Cannot find matching port.');
-		}
-	})
+  serialport.list(function(err, ports) {
+    var found = ports.some(function(port) {
+      if(port.manufacturer == 'Roboteq') {
+        cb(null, port.comName);
+        return true;        
+      } else {
+        return false;
+      }
+    })
+    if(!found) {
+      cb('Cannot find matching port.');
+    }
+  })
 }
 
 var config = {};
 var serial;
 var serialStatus = 'initializing';
 function updateSerialStatus(status) {
-	if(status != serialStatus) {
-		console.log('Roboteq is ' + status);
-	}
-	serialStatus = status;
+  if(status != serialStatus) {
+    console.log('Roboteq is ' + status);
+  }
+  serialStatus = status;
 }
 
 var dataCallbackHooks = [];
 function dataCallback(data) {
-	if(data == '+') {
-		console.log('dataCallback: no reply expected');
-		if(dataCallbackHooks.length > 0) {
-			dataCallbackHooks.pop();
-		}
-		return;
-	}
-	if(data == '-') {
-		console.log('dataCallback: bad command');
-		if(dataCallbackHooks.length > 0) {
-			dataCallbackHooks.pop();
-		}
-		return;
-	}
-	console.log('dataCallback: ' + data);
-	if(dataCallbackHooks.length > 0) {
-		// console.log("deleting callback hook");
-		dataCallbackHooks.pop()(data);
-	}
+  if(data == '+') {
+    // console.log('dataCallback: no reply expected');
+    if(dataCallbackHooks.length > 0) {
+      dataCallbackHooks.pop();
+    }
+    return;
+  }
+  if(data == '-') {
+    console.log('dataCallback: bad command');
+    if(dataCallbackHooks.length > 0) {
+      dataCallbackHooks.pop();
+    }
+    return;
+  }
+  // console.log('dataCallback: ' + data);
+  if(dataCallbackHooks.length > 0) {
+    // console.log("deleting callback hook");
+    dataCallbackHooks.pop()(data);
+  }
 }
 
 exports.connect = function(params) {
-	if(params) config = params;
-	if(serial && serial.isOpen()) return;
-	getComName(function(err, comName) {
-		if(err) {
-			updateSerialStatus('not found, searching');
-			reconnect();
-			return;
-		}
-		serial = new serialport.SerialPort(comName, {
-			baudrate: 115200,
-			parser: serialport.parsers.readline('\r'),
-			disconnectedCallback: function() {
-				updateSerialStatus('disconnected, reconnecting');
-				reconnect();
-			}
-		}, false);
-		serial.on('data', dataCallback);
-		serial.open(function(err) {
-			if(err) {
-				updateSerialStatus('error connecting, reconnecting');
-				reconnect();
-			} else {
-				updateSerialStatus('connected');
-				exports.setEcho(false);
-			}
-		});
-	})
+  if(params) config = params;
+  if(serial && serial.isOpen()) return;
+  getComName(function(err, comName) {
+    if(err) {
+      updateSerialStatus('not found, searching');
+      reconnect();
+      return;
+    }
+    serial = new serialport.SerialPort(comName, {
+      baudrate: 115200,
+      parser: serialport.parsers.readline('\r'),
+      disconnectedCallback: function() {
+        updateSerialStatus('disconnected, reconnecting');
+        reconnect();
+      }
+    }, false);
+    serial.on('data', dataCallback);
+    serial.open(function(err) {
+      if(err) {
+        updateSerialStatus('error connecting, reconnecting');
+        reconnect();
+      } else {
+        updateSerialStatus('connected');
+        exports.setEcho(false);
+      }
+    });
+  })
 }
 function reconnect(description) {
-	setTimeout(exports.connect, 1000);
+  setTimeout(exports.connect, 1000);
 }
 
 exports.isOpen = function() {
-	return Boolean(serial && serial.isOpen());
+  return Boolean(serial && serial.isOpen());
 }
 
 exports.serialStatus = function() {
-	return serialStatus;
+  return serialStatus;
 }
 
 exports.command = function(command) {
-	if(!exports.isOpen()) {
-		console.log('Ignored command: ' + command);
-		return;
-	}
-	write(command + '\r');
+  if(!exports.isOpen()) {
+    console.log('Ignored command: ' + command);
+    return;
+  }
+  write(command + '\r');
 }
 
 function write(command, cb) {
-	if(cb) {
-		dataCallbackHooks.push(cb);
-	}
-	console.log('write: ' + command);
-	serial.write(command, function(err) {
-		if(err) console.log('err ' + err);    
-	})
+  if(cb) {
+    dataCallbackHooks.push(cb);
+  }
+  // console.log('write: ' + command);
+  serial.write(command, function(err) {
+    if(err) console.log('err ' + err);    
+  })
 }
 
 exports.query = function(query, cb) {
-	if(!exports.isOpen()) {
-		console.log('Ignored query: ' + query);
-		cb();
-		return;
-	}
-	write(query + '\r', function(data) {
-		var parts = data.split('=');
-		var number = Number(parts[1]); // this will fail if a command does not return a number
-		cb(number);
-	});
+  if(!exports.isOpen()) {
+    console.log('Ignored query: ' + query);
+    cb();
+    return;
+  }
+  write(query + '\r', function(data) {
+    var parts = data.split('=');
+    var number = Number(parts[1]); // this will fail if a command does not return a number
+    cb(number);
+  });
 }
 
 function unsafe(x) {
-	if(x === undefined || x == null || isNaN(x)) {
-		console.log("unsafe value passed to roboteq: " + x);
-		return true;
-	}
-	return false;
+  if(x === undefined || x == null || isNaN(x)) {
+    console.log('unsafe value passed to roboteq: ' + x);
+    return true;
+  }
+  return false;
 }
 
 // based on:
@@ -147,86 +147,86 @@ function unsafe(x) {
 // - for commands where no reply is expected, it will return a '+' character
 // - for bad commands, it will return a '-' character
 exports.safeCall = function(cb) {
-	cb();
-	// exports.getMotorAmps(function(amps) {
-	// 	if(amps > 0) {
-	// 		cb();
-	// 	}
-	// })
+  cb();
+  // exports.getMotorAmps(function(amps) {
+  //  if(amps > 0) {
+  //    cb();
+  //  }
+  // })
 }
 exports.setEcho = function(enable) {
-	if(enable) {
-		exports.command('^echof 0');
-	} else {
-		exports.command('^echof 1');
-	}
+  if(enable) {
+    exports.command('^echof 0');
+  } else {
+    exports.command('^echof 1');
+  }
 }
 exports.setAcceleration = function(acceleration) {
-	if(unsafe(acceleration)) return;
-	acceleration = clamp(acceleration, 0, config.accelerationLimit);
-	exports.command('!ac 1 ' + acceleration);
+  if(unsafe(acceleration)) return;
+  acceleration = clamp(acceleration, 0, config.accelerationLimit);
+  exports.command('!ac 1 ' + acceleration);
 }
 exports.setDeceleration = function(deceleration) {
-	if(unsafe(deceleration)) return;
-	deceleration = clamp(deceleration, 0, config.decelerationLimit);
-	exports.command('!dc 1 ' + deceleration);
+  if(unsafe(deceleration)) return;
+  deceleration = clamp(deceleration, 0, config.decelerationLimit);
+  exports.command('!dc 1 ' + deceleration);
 }
 exports.setSpeed = function(speed) { // units are .1 * RPM / s, called "set velocity" in manual
-	if(unsafe(speed)) return;
-	speed = clamp(speed, 0, config.speedLimit);
-	exports.command('!s 1 ' + speed);
+  if(unsafe(speed)) return;
+  speed = clamp(speed, 0, config.speedLimit);
+  exports.command('!s 1 ' + speed);
 }
 exports.setPosition = function(position) {
-	if(unsafe(position)) return;
-	exports.safeCall(function() {
-		position = clamp(position, config.bottom, config.top);
-		exports.command('!p 1 ' + position);
-	})
+  if(unsafe(position)) return;
+  exports.safeCall(function() {
+    position = clamp(position, config.bottom, config.top);
+    exports.command('!p 1 ' + position);
+  })
 }
 exports.setPositionRelative = function(distance) {
-	if(unsafe(distance)) return;
-	exports.getPosition(function(result) {
-		console.log('setPositionRelative to ' + distance + ' plus ' + result);
-		exports.setPosition(result + Number(distance));
-	})
+  if(unsafe(distance)) return;
+  exports.getPosition(function(result) {
+    // console.log('setPositionRelative to ' + distance + ' plus ' + result);
+    exports.setPosition(result + Number(distance));
+  })
 }
 exports.getPosition = function(cb) {
-	exports.query('?c 1', cb); // also called "encoder counter absolute"
+  exports.query('?c 1', cb); // also called "encoder counter absolute"
 }
 exports.getSpeed = function(cb) { // units are .1 * RPM / s, called "set velocity" in manual
-	exports.query('?s 1', cb);
+  exports.query('?s 1', cb);
 }
 // returns voltage * 10 : main battery voltage * 10 : v5out on dsub in millivolts, see p 186
 // ?v 1 returns internal voltage
 // ?v 2 returns motor voltage
 // ?v 3 return 5v on dsub output
 exports.getVolts = function(cb) {
-	exports.query('?v 2', function(volts) {
-		cb(volts / 10.);
-	});
+  exports.query('?v 2', function(volts) {
+    cb(volts / 10.);
+  });
 }
 exports.getMotorAmps = function(cb) { // returns units of amps * 10, p 173
-	exports.query('?A 1', function(amps) {
-		cb(amps / 10.); 
-	});
+  exports.query('?A 1', function(amps) {
+    cb(amps / 10.); 
+  });
 }
 exports.getBatteryAmps = function(cb) { // returns units of amps * 10, p 175
-	exports.query('?BA 1', function(amps) {
-		cb(amps / 10.);
-	})
+  exports.query('?BA 1', function(amps) {
+    cb(amps / 10.);
+  })
 }
 exports.getDestinationReached = function(cb) { // p 179, p 104
-	exports.query('?DR 1', cb);
+  exports.query('?DR 1', cb);
 }
 
 // follow commands are untested
 exports.getFaults = function(cb) {
-	exports.query('?FF 1', cb); // see p 180 for the meaning of each bit
+  exports.query('?FF 1', cb); // see p 180 for the meaning of each bit
 }
 exports.getRuntimeStatus = function(cb) {
-	exports.query('?FM 1', cb); // see p 181 for the meaning of each bit
+  exports.query('?FM 1', cb); // see p 181 for the meaning of each bit
 }
 exports.getStatus = function(cb) {
-	exports.query('?FS 1', cb); // see p 181 for the meaning of each bit
+  exports.query('?FS 1', cb); // see p 181 for the meaning of each bit
 }
 // see p 188 and 189 for a way to set up the roboteq to automatically return stats
